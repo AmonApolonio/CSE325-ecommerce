68ae2a4ae0471bcd62f33848a7cb79ea609042d0
Amon Apolonio <amonapolonio.dev@gmail.com>
2025-11-17 21:37:03 -0300

    feat: initial wireframe for the homescreen

--- frontend/Program.cs
+++ frontend/Program.cs
@@ -1,14 +1,20 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using frontend;
+using frontend.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+<<<<<<< HEAD
 builder.Services.AddScoped(sp => new HttpClient
 {
     BaseAddress = new Uri("https://localhost:5028") 
 });
+=======
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<IProductService, ProductService>();
+>>>>>>> a52d2c9 (feat: initial wireframe for the homescreen)
 
 await builder.Build().RunAsync();
